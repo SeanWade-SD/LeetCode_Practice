758505d495ad625c113d15c2ef20abe964f1cafc
SeanWade-SD <yiwei.xue98@outlook.com>
2020-11-23 23:21:44 +0000

    23/11/2020

--- Sorted_Merge_LCCI.py
+++ Sorted_Merge_LCCI.py
@@ -38,3 +38,48 @@ class Solution1:
         A[m:] = B
         A.sort()
 
+
+class Solution2:
+    def merge(self, A, m, B, n):
+        """
+        Do not return anything, modify A in-place instead.
+        """
+        sorted = []
+        pa, pb = 0, 0
+        while pa < m or pb < n:
+            if pa == m:
+                sorted.append(B[pb])
+                pb += 1
+            elif pb == n:
+                sorted.append(A[pa])
+                pa += 1
+            elif A[pa] < B[pb]:
+                sorted.append(A[pa])
+                pa += 1
+            else:
+                sorted.append(B[pb])
+                pb += 1
+        A[:] = sorted
+
+
+class Solution3:
+    def merge(self, A, m, B, n):
+        """
+        Do not return anything, modify A in-place instead.
+        """
+        pa, pb = m - 1, n - 1
+        tail = m + n - 1
+        while pa >= 0 or pb >= 0:
+            if pa == -1:
+                A[tail] = B[pb]
+                pb -= 1
+            elif pb == -1:
+                A[tail] = A[pa]
+                pa -= 1
+            elif A[pa] > B[pb]:
+                A[tail] = A[pa]
+                pa -= 1
+            else:
+                A[tail] = B[pb]
+                pb -= 1
+            tail -= 1
